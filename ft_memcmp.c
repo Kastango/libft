52a5821438a55de08db75101c4b6d84b6cbaa178
arbernar <arbernar@student.42sp.org.br>
2021-09-18 20:09:59 -0300

    tests memcmp

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,26 +6,34 @@
 /*   By: arbernar <arbernar@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/10 17:09:53 by arbernar          #+#    #+#             */
-/*   Updated: 2021/09/18 20:07:16 by arbernar         ###   ########.fr       */
+/*   Updated: 2021/09/18 20:09:39 by arbernar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_memcmp(const void *b1, const void *b2, size_t n)
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-	unsigned char	*s1;
-	unsigned char	*s2;
-	size_t			i;
+	size_t				i;
+	const unsigned char	*str1;
+	const unsigned char	*str2;
 
-	s1 = (unsigned char *)b1;
-	s2 = (unsigned char *)b2;
+	if (n == 0 || s1 == s2)
+		return (0);
+	str1 = (const unsigned char *)s1;
+	str2 = (const unsigned char *)s2;
 	i = 0;
-	while (n--)
+	while (str1[i] == str2[i] && i < n)
 	{
-		if (s1[i] != s2[i])
-			return (s1[i] - s2[i]);
-		i++;
+		if (i + 1 < n)
+			i++;
+		else
+			return (0);
 	}
-	return (0);
+	if (str1[i] - str2[i] > 0)
+		return (1);
+	else if (str1[i] - str2[i] < 0)
+		return (-1);
+	else
+		return (0);
 }
