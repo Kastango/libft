2e1205c1b3ae2790abd471896dc1b47ad377c846
arbernar <arbernar@student.42sp.org.br>
2021-09-19 16:51:54 -0300

    problems with lstmap

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: arbernar <arbernar@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/19 14:50:10 by arbernar          #+#    #+#             */
-/*   Updated: 2021/09/19 16:08:16 by arbernar         ###   ########.fr       */
+/*   Updated: 2021/09/19 16:51:26 by arbernar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,25 +14,25 @@
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-	t_list	*new_lst;
+	t_list	*new_entry;
 	t_list	*first;
 
-	if (!lst || !f || !del)
+	if (!lst)
 		return (NULL);
 	first = ft_lstnew(f(lst->content));
 	if (!first)
 		return (NULL);
-	new_lst = first;
+	new_entry = first;
 	lst = lst->next;
 	while (lst != NULL)
 	{
-		new_lst->next = ft_lstnew(f(lst->content));
-		if (new_lst->next == NULL)
+		new_entry->next = ft_lstnew(f(lst->content));
+		if (new_entry->next == NULL)
 		{
 			ft_lstclear(&first, del);
 			return (NULL);
 		}
-		new_lst = new_lst->next;
+		new_entry = new_entry->next;
 		lst = lst->next;
 	}
 	return (first);
